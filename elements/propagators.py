092b6b6c2355072a70fdc39986bfaf6917080b63
Belashov641618 <uclaptrap@gmail.com>
2024-04-25 02:18:08 +0300

    Модель обучается 2

--- elements/propagators.py
+++ elements/propagators.py
@@ -3,6 +3,65 @@ import torch
 from utilities import *
 from .abstracts import AbstractPropagator
 
+class ConvolutionalPropagation(AbstractPropagator):
+
+    @property
+    def _convolution_paddings(self):
+        left = self._total_pixels_x // 2
+        top = self._total_pixels_y // 2
+        return [left, top]
+
+    def _recalc_propagation_buffer(self):
+        freq_x = torch.fft.fftfreq(self._total_pixels_x, d=self._step_x, device=self.device, dtype=self.accuracy.tensor_float)
+        freq_y = torch.fft.fftfreq(self._total_pixels_y, d=self._step_y, device=self.device, dtype=self.accuracy.tensor_float)
+        freq_x_mesh, freq_y_mesh = torch.meshgrid(freq_x, freq_y, indexing='ij')
+
+        wavelength = self.wavelength.tensor.reshape(-1,1,1).to(self.device)
+        reflection = (self.reflection.tensor + 1j*self.absorption.tensor).reshape(-1,1,1).to(self.device)
+
+        K2 = ((1.0 / (wavelength * reflection))**2)
+        Kz = (2. * torch.pi * torch.sqrt(K2 - freq_x_mesh**2 - freq_y_mesh**2))
+        propagator = torch.exp(1j * Kz * self.distance)
+
+        propagator = torch.fft.ifft2(propagator)
+
+        self._register_propagation_buffer(propagator)
+
+    def _reset_all(self):
+        self.delayed.add(self._reset_add_pixels, -1.)
+        self.delayed.add(self._recalc_propagation_buffer)
+    def _change_pixels(self):
+        self._reset_all()
+    def _change_length(self):
+        self._reset_all()
+    def _change_distance(self):
+        self.delayed.add(self._recalc_propagation_buffer)
+    def _change_absorption(self):
+        self.delayed.add(self._recalc_propagation_buffer)
+    def _change_reflection(self):
+        self.delayed.add(self._recalc_propagation_buffer)
+    def _change_wavelength(self):
+        self.delayed.add(self._recalc_propagation_buffer)
+
+    def __init__(self, pixels:IntIO, length:FloatIO, wavelength:FloatS, reflection:FloatS, absorption:FloatS, distance:float, logger:Logger=None):
+        super().__init__(pixels, length, wavelength, reflection, absorption, distance, logger=logger)
+        self._reset_all()
+        self.delayed.launch()
+
+    def forward(self, field:torch.Tensor, *args, **kwargs):
+        super().forward(field,*args, **kwargs)
+
+        field = torch.nn.functional.pad(field, self._paddings)
+
+        field = torch.nn.functional.conv2d(field, self._propagation_buffer.expand(field.shape[1], 1, *self._propagation_buffer.shape[1:]), groups=field.shape[1], padding=self._convolution_paddings)
+
+        field = torch.nn.functional.pad(field, self._unpaddings)
+        field = interpolate(field, (self.pixels.output.x, self.pixels.output.y), mode=self.interpolation.mode)
+
+        return field
+
+
+
 class FurrierPropagation(AbstractPropagator):
     _pad_pixels:XYParams[int]
     def _reset_pad_pixels(self):
@@ -31,8 +90,9 @@ class FurrierPropagation(AbstractPropagator):
 
         K2 = ((1.0 / (wavelength * reflection))**2)
         Kz = (2. * torch.pi * torch.sqrt(K2 - freq_x_mesh**2 - freq_y_mesh**2))
+        propagator = torch.exp(1j * Kz * self.distance)
 
-        self._register_propagation_buffer(torch.exp(1j * Kz * self.distance))
+        self._register_propagation_buffer(propagator)
 
     def _change_border(self):
         self.delayed.add(self._reset_pad_pixels, -1.)
@@ -68,10 +128,12 @@ class FurrierPropagation(AbstractPropagator):
 
     def forward(self, field:torch.Tensor, *args, **kwargs):
         super().forward(field,*args, **kwargs)
+
         field = torch.nn.functional.pad(field, self._paddings)
         field = torch.fft.fft2(field)
         field = field * self._propagation_buffer
         field = torch.fft.ifft2(field)
         field = torch.nn.functional.pad(field, self._unpaddings)
         field = interpolate(field, (self.pixels.output.x, self.pixels.output.y), mode=self.interpolation.mode)
+
         return field
